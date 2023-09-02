bcadbc34ab9a332eb2763fd80a82748b7f81505e
kietAIoT <kiet.nguyencmis1@gmail.com>
2023-09-02 17:40:22 +0700

    first push

---
+++ Debug/sources.mk
@@ -0,0 +1,31 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (11.3.rel1)
+################################################################################
+
+ELF_SRCS := 
+OBJ_SRCS := 
+S_SRCS := 
+C_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+CYCLO_FILES := 
+OBJDUMP_LIST := 
+S_DEPS := 
+OBJCOPY_SREC := 
+C_DEPS := 
+OBJCOPY_BIN := 
+OBJCOPY_HEX := 
+SIZE_OUTPUT := 
+SU_FILES := 
+EXECUTABLES := 
+OBJS := 
+MAP_FILES := 
+S_UPPER_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+Core/Src \
+Core/Startup \
+Drivers/STM32F1xx_HAL_Driver/Src \
+
